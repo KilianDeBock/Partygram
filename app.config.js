290c878bdf2b1f3a576a35d8a79c3917c2f7ac30
Kilian De Bock <kilian.debock@student.arteveldehs.be>
2022-12-16 11:00:16 +0100

    22.50.5.1: Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,10 +1,9 @@
 import "dotenv/config";
 
-module.exports = {
-  name: "Partygram",
-  version: "1.0.0",
+module.exports = ({ config }) => ({
+  ...config,
   extra: {
     SUPABASE_URL: process.env.SUPABASE_URL,
     SUPABASE_KEY: process.env.SUPABASE_KEY,
   },
-};
+});
