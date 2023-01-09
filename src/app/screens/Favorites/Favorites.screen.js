aca96900c582368b56167801d0d8933e7f4166cf
Kilian De Bock <kilian.debock@student.arteveldehs.be>
2023-01-09 11:08:06 +0100

    23.2.1.6: Finish Favorites

--- src/app/screens/Favorites/Favorites.screen.js
+++ src/app/screens/Favorites/Favorites.screen.js
@@ -1,1 +1,18 @@
-export const FavoritesScreen = () => {};
+import { PostDesignComponent } from "../../components/design/Post/Post.design.component";
+import DefaultView from "../../components/design/View/DefaultView.design.component";
+import DataListView from "../../components/shared/Data/DataListView.shared.component";
+
+export const FavoritesScreen = () => {
+  return (
+    <DefaultView padding={false}>
+      <DataListView
+        name={["posts"]}
+        method={() => ({ data: ["1", "2", "3", "4", "5"] })}
+        emptyTitle={"No posts"}
+        emptyDescription={"You have no posts."}
+        emptyIcon="folder"
+        renderItem={({ item }) => <PostDesignComponent />}
+      />
+    </DefaultView>
+  );
+};
