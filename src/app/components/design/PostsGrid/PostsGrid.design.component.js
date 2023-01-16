260777ff2572b91f9556eef7a609d901b4e6da33
Kilian De Bock <kilian.debock@student.arteveldehs.be>
2023-01-16 10:24:28 +0100

    23.3.1.3: Fix empty lists

--- src/app/components/design/PostsGrid/PostsGrid.design.component.js
+++ src/app/components/design/PostsGrid/PostsGrid.design.component.js
@@ -2,21 +2,26 @@ import { FlatList, Image, StyleSheet, View } from "react-native";
 import { AddPostDialog } from "../../shared/AddPostDialog/AddPostDialog.shared.component";
 
 export const PostsGridDesignComponent = ({ posts, addDialog = false }) => {
+  const list = posts.length < 1 ? [{ id: 0 }] : posts;
+  const listEmpty = posts.length < 1;
+
   return (
     <FlatList
       contentContainerStyle={styles.container}
-      data={posts}
+      data={list}
       renderItem={({ item, index }) => (
         <>
           {addDialog && index === 0 && <AddPostDialog />}
-          <View style={styles.imageView}>
-            <Image
-              style={styles.image}
-              source={{
-                uri: `https://jvrcjuipyagwvwalcpzo.supabase.co/storage/v1/object/public/posts/${item.image}`,
-              }}
-            />
-          </View>
+          {!listEmpty && (
+            <View style={styles.imageView}>
+              <Image
+                style={styles.image}
+                source={{
+                  uri: `https://jvrcjuipyagwvwalcpzo.supabase.co/storage/v1/object/public/posts/${item.image}`,
+                }}
+              />
+            </View>
+          )}
         </>
       )}
       numColumns={3}
