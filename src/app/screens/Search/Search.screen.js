ffcdbbe652cf4534c86d7cff60dd2fff77f49181
Kilian De Bock <kilian.debock@student.arteveldehs.be>
2023-01-16 11:06:44 +0100

    23.3.1.9: Make search more reactive

--- src/app/screens/Search/Search.screen.js
+++ src/app/screens/Search/Search.screen.js
@@ -1,11 +1,11 @@
 import TextField from "../../components/design/Form/TextField.design.component";
 import { PostsGridDesignComponent } from "../../components/design/PostsGrid/PostsGrid.design.component";
 import DefaultView from "../../components/design/View/DefaultView.design.component";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { searchPosts } from "../../../core/modules/post/api";
 
 export const SearchScreen = () => {
-  const [cooldown, setCooldown] = useState(null);
+  const [text, setText] = useState(null);
   const [posts, setPosts] = useState([]);
 
   const search = async (search) => {
@@ -14,23 +14,14 @@ export const SearchScreen = () => {
     setPosts(p);
   };
 
-  const handleOnChangeText = (text) => {
-    // Clear the timeout if it exists
-    if (cooldown) {
-      clearTimeout(cooldown);
-      setCooldown(null);
-    }
-
-    // Check if the length is greater than 3
-    if (text.length < 3) return;
-
-    // Set a timeout to search
-    setCooldown(setTimeout(() => search(text), 500));
-  };
+  useEffect(() => {
+    const id = setTimeout(() => search(text), 500);
+    return () => clearTimeout(id);
+  }, [text]);
 
   return (
     <DefaultView padding={false}>
-      <TextField onChangeText={handleOnChangeText} placeholder={"#hello"} />
+      <TextField onChangeText={setText} placeholder={"#hello"} />
       <PostsGridDesignComponent posts={posts} />
     </DefaultView>
   );
