0c3073faef2a58dc7476ca7249d140b996e90f0a
Kilian De Bock <kilian.debock@student.arteveldehs.be>
2023-01-23 14:31:24 +0100

    23.4.1.12: Setup EAS

--- app.config.js
+++ app.config.js
@@ -5,5 +5,8 @@ module.exports = ({ config }) => ({
   extra: {
     SUPABASE_URL: process.env.SUPABASE_URL,
     SUPABASE_KEY: process.env.SUPABASE_KEY,
+    eas: {
+      projectId: "89ed8ae4-9650-4974-be17-98508b7d7a0c",
+    },
   },
 });
