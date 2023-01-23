608bd52f3623b05311aede82fb21126bbbf81ff0
Kilian De Bock <kilian.debock@student.arteveldehs.be>
2023-01-23 16:48:39 +0100

    23.4.1.16: Fix even more bugs

--- src/app/screens/Home/Story.screen.js
+++ src/app/screens/Home/Story.screen.js
@@ -5,6 +5,7 @@ import { Image, Pressable, StyleSheet, Text } from "react-native";
 import { useEffect, useState } from "react";
 import { getPublicUrl } from "../../../core/modules/files/utils";
 import { Navigation } from "../../../core/navigation";
+import LoadingIndicator from "../../components/design/Loading/LoadingIndicator.design.component";
 
 export const StoryScreen = ({ route, navigation }) => {
   const { item: selectItem } = route.params;
@@ -16,8 +17,10 @@ export const StoryScreen = ({ route, navigation }) => {
 
   let startIndex =
     selectItem !== undefined && selectItem !== null
-      ? stories.findIndex((i) => i.id === selectItem.id) ?? 0
+      ? stories.findIndex((i) => i.id === selectItem?.id) ?? 0
       : 0;
+
+  console.log("startIndex", startIndex, typeof startIndex);
 
   const [index, setIndex] = useState(startIndex);
 
@@ -27,53 +30,56 @@ export const StoryScreen = ({ route, navigation }) => {
       return navigation.navigate(Navigation.HOME_OVERVIEW);
     }
     const newStory = stories[ind];
-    const img = { uri: getPublicUrl("posts", newStory.image) };
+    const img = { uri: getPublicUrl("posts", newStory?.image) };
     setStory({ ...newStory, image: img });
     setIndex(ind + 1);
   };
 
   useEffect(() => {
-    if (!data || !stories || stories.length < 1 || data.error) return () => {};
+    if (!!data && !!stories && stories.length > 0 && !data?.error) {
+      setTimeLeft(5);
+      const timeout = setTimeout(() => {
+        if (index < stories?.length) {
+          nextImage(stories[index]);
+        } else {
+          navigation.navigate(Navigation.HOME_OVERVIEW);
+        }
+      }, 5000);
 
-    setTimeLeft(5);
-    const timeout = setTimeout(() => {
-      if (index < stories.length) {
-        nextImage(stories[index]);
-      } else {
-        navigation.navigate(Navigation.HOME_OVERVIEW);
-      }
-    }, 5000);
+      const interval = setInterval(() => {
+        setTimeLeft((timeLeft) => timeLeft - 1);
+      }, 1000);
 
-    const interval = setInterval(() => {
-      setTimeLeft((timeLeft) => timeLeft - 1);
-    }, 1000);
-
-    return () => {
-      clearInterval(interval);
-      clearTimeout(timeout);
-    };
+      return () => {
+        clearInterval(interval);
+        clearTimeout(timeout);
+      };
+    }
   }, [index]);
 
   useEffect(() => {
-    if (!data || !stories || stories.length < 1 || data.error) return () => {};
-
-    nextImage();
+    if (!!data && !!stories && stories.length > 0 && !data?.error) {
+      nextImage();
+    }
   }, [data]);
 
-  if (!story) return null;
-  const user = story.user_profiles;
-  const userStories = stories.filter((i) => i.user_id === user.auth);
-  const userStoriesLeft = userStories.filter((i) => i.id <= story.id);
+  if (!data || !stories || stories.length < 1 || !story)
+    return <LoadingIndicator />;
+  const user = story?.user_profiles;
+  const userStories = stories?.filter((i) => i?.user_id === user?.auth);
+  const userStoriesLeft = userStories?.filter((i) => i.id <= story?.id);
+
+  if (!user || !userStories || !userStoriesLeft) return <LoadingIndicator />;
 
   return (
     <DefaultView padding={false}>
       <Pressable onPress={nextImage}>
         <Text>{timeLeft > 5 ? 0 : timeLeft} seconds left!</Text>
         <Text>
-          Viewing a story from {user.firstname} {user.lastname} ({user.username}
-          ) ({userStoriesLeft.length}/{userStories.length})
+          Viewing a story from {user?.firstname} {user?.lastname} (
+          {user?.username}) ({userStoriesLeft?.length}/{userStories?.length})
         </Text>
-        <Image source={story.image} style={styles.image} />
+        <Image source={story?.image} style={styles?.image} />
       </Pressable>
     </DefaultView>
   );
